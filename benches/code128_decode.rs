68703e20efe592d91332ba160bb162e75849e85e
Gwen Lofman <Gwen@Lofman.co>
2017-07-22 16:17:11 -0400

    Add rudimentary benchmark tests
    Benchmark testing allows me to check for performance regressions and
    make performance improvements over time.  Essential for having real
    time capability.

---
+++ benches/code128_decode.rs
@@ -0,0 +1,40 @@
+#![feature(test)]
+
+extern crate feather_code as fc;
+extern crate test;
+
+use fc::internals::code128::Code128;
+use fc::internals::format::Decode;
+use fc::internals::code128::encodings;
+use test::Bencher;
+
+#[bench]
+fn decode_u8(bencher: &mut Bencher) {
+    let buffer = [105, 102, 42, 18, 40, 20, 50, 101, 16, 92, 106];
+    let code = Code128(buffer.as_ref());
+
+    bencher.iter(|| {
+        code.decode().unwrap();
+    })
+}
+
+#[bench]
+fn decode_vec(bencher: &mut Bencher) {
+    let buffer = vec![105, 102, 42, 18, 40, 20, 50, 101, 16, 92, 106];
+    let code = Code128(buffer.as_ref());
+
+    bencher.iter(|| {
+        code.decode().unwrap();
+    })
+}
+
+#[bench]
+fn decode_pattern(bencher: &mut Bencher) {
+    use encodings::Pattern::*;
+    let buffer = [C105, C102, C42, C18, C40, C20, C50, C101, C16, C92, C106];
+    let code = Code128(buffer.as_ref());
+
+    bencher.iter(|| {
+        code.decode().unwrap();
+    })
+}
